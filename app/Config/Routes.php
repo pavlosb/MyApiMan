6af0feb7c6216101d092b457a3d5d7bf310b271e
pavlosb <pavlos.bizimis@gmail.com>
2023-10-27 15:00:56 +0300

    api test

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('testme', 'Home::testme');
+$routes->get('autoapi', 'Auto::index');
---
+++ app/Controllers/Auto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Auto extends BaseController
+{
+    public function index(): string
+    {
+        echo "For Autotask Api";
+    }
+
+    
+}
