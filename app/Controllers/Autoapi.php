ac65155920c5500be0549d7e669c3771c391d998
pavlosb <pavlos.bizimis@gmail.com>
2023-10-29 20:57:15 +0200

    Revert "Update Autoapi.php"
    This reverts commit 949958e294bd76eef387aebf409059b09ac1f6fd.

--- app/Controllers/Autoapi.php
+++ app/Controllers/Autoapi.php
@@ -4,39 +4,26 @@ namespace App\Controllers;
 
 class Autoapi extends BaseController
 {
-    protected $apiuri;
-    protected $apiic;
-    protected $apiuser;
-    protected $apipass;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->apiic = '5IXBPE3XZTKWTF4562735DXFAAA';
-        $this->apiuri = 'https://webservices19.autotask.net/atservicesrest/v1.0/';
-        $this->apiuser = 'dsehfrek2bzrqfw@inline.gr';
-        $this->apipass = 'M#z7c3$ZP*t0dW4@N~x69E@wn';
-
-    }
+    
     public function index()
     {
-        echo $this->apiic;
         
-        //$client = \Config\Services::curlrequest();
+        
+        $client = \Config\Services::curlrequest();
         $headerData = array(
-            'ApiIntegrationCode' => $this->apiic,
-            'UserName' => $this->apiuser,
-            'Secret'   => $this->apipass,
+            'ApiIntegrationCode' => '5IXBPE3XZTKWTF4562735DXFAAA',
+            'UserName' => 'dsehfrek2bzrqfw@inline.gr',
+            'Secret'   => 'M#z7c3$ZP*t0dW4@N~x69E@wn',
             'Connection' => 'Keep-Alive',
             'Accept-Encoding' => 'gzip, deflate, br',
             'Content-Type' => 'application/json',
         );
 
         $apiURL = 'https://webservices19.autotask.net/atservicesrest/v1.0/Companies/query?search={"filter":[{"op":"in","field":"CompanyType","value":[1,3,7]}]}';
-        //$response = $client->get($apiURL,[
-       //     'debug' => true,
-       //     'headers'=>$headerData,
-       // ]);
+      $response = $client->get($apiURL,[
+           'debug' => true,
+           'headers'=>$headerData,
+       ]);
 
     // Read response
     $code = $response->getStatusCode();
